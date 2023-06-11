ac7f224e7a592a8d322c90f3575e058f8633b9e5
Matias Di Gialleonardo <matiasdigialleonardo@gmail.com>
2023-06-10 22:21:14 -0300

    Moved the update functionality to the update module, and the getting divs and creating li elements funcionality to the utils module.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
 export function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function createListElement(content) {
+  const li = document.createElement("li");
+  li.textContent = content;
+  return li;
+}
+
+export function getDiv(divId) {
+  return document.getElementById(divId);
+}
